01ba1d77c754b81caf5d470564e99df41db9f4ce
cafe2404 <minhphung8898@gmail.com>
2024-12-24 03:53:14 +0700

    update

--- landing/context_processors.py
+++ landing/context_processors.py
@@ -3,7 +3,7 @@ def landing_context(request):
     facebook = SocialLink.objects.filter(is_active=True, platform='Facebook').first()
     telegram = SocialLink.objects.filter(is_active=True, platform='Telegram').first()
     zalo = SocialLink.objects.filter(is_active=True, platform='Zalo').first()
-    landing = LandingPageContent.objects.get(is_active=True)
+    landing = LandingPageContent.objects.filter(is_active=True).order_by('-id').first()
     return {
         'facebook': facebook,
         'telegram': telegram,
