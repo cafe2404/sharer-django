c25cadb050a8400c840ccdfe7f581ae1eeabf9d7
cafe2404 <minhphung8898@gmail.com>
2024-12-25 02:55:41 +0700

    fixx bug websocket

--- sharer/asgi.py
+++ sharer/asgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sharer.settings')
 django.setup()
 django_asgi_app = get_asgi_application()
@@ -8,7 +9,6 @@ django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from orders import routing  # Đảm bảo import routing từ sharer
-import django
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
